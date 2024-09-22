f168e5f86a5a8a9e4dd5a412e5271dcca0e9435c
WalterMijangos <walter24mijangosp@gmail.com>
2024-09-21 21:07:51 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,42 @@
+﻿using ApiPersonajes.Models;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+namespace ApiPersonajes.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+        private HttpClient _httpClient;
+        public CharactersController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<Characters> GetAllCharacters(int page, string name = null)
+        {
+            try
+            {
+                Characters characters = null;
+                string url = string.IsNullOrEmpty(name) ?  $"https://rickandmortyapi.com/api/character?page={page}"
+					: $"https://rickandmortyapi.com/api/character?page={page}&name={name}";
+
+				HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync(url);
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+
+                return characters;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Models/Characters.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiPersonajes.Models
+{
+    public class Characters
+    {
+        public Info Info { get; set; }
+         
+        public List<Character> results { get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+}
